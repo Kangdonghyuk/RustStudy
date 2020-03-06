917248095f3d595710dd438737ed5e5cacb74743
Kangdonghyuk <kdh950812@gmail.com>
2020-03-06 16:01:14 +0900

    0306-add if,for

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/liunx/Desktop/rustex/branches/target/debug/branches: /home/liunx/Desktop/rustex/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-75622e66d5be13b6.d
@@ -0,0 +1,5 @@
+/home/liunx/Desktop/rustex/branches/target/debug/deps/branches-75622e66d5be13b6: src/main.rs
+
+/home/liunx/Desktop/rustex/branches/target/debug/deps/branches-75622e66d5be13b6.d: src/main.rs
+
+src/main.rs:
