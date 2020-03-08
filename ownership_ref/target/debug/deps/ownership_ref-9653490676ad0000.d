e3b38e9f2a396518d4c81329ead6268cf24230ea
Kangdonghyuk <kdh950812@gmail.com>
2020-03-08 19:40:51 +0900

    0308-add ref

---
+++ ownership_ref/target/debug/deps/ownership_ref-9653490676ad0000.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/ownership_ref/target/debug/deps/ownership_ref-9653490676ad0000: src/main.rs
+
+/mnt/c/Users/kdh95/Desktop/RustStudy/ownership_ref/target/debug/deps/ownership_ref-9653490676ad0000.d: src/main.rs
+
+src/main.rs:
---
+++ ownership_ref/target/debug/ownership_ref.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/ownership_ref/target/debug/ownership_ref: /mnt/c/Users/kdh95/Desktop/RustStudy/ownership_ref/src/main.rs
