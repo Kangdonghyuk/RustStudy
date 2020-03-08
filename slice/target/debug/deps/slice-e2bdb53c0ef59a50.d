d2244e78f145311482dd360781206c378621ccfb
Kangdonghyuk <kdh950812@gmail.com>
2020-03-08 20:17:29 +0900

    0308-add slice

---
+++ slice/target/debug/deps/slice-e2bdb53c0ef59a50.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/slice/target/debug/deps/slice-e2bdb53c0ef59a50: src/main.rs
+
+/mnt/c/Users/kdh95/Desktop/RustStudy/slice/target/debug/deps/slice-e2bdb53c0ef59a50.d: src/main.rs
+
+src/main.rs:
---
+++ slice/target/debug/slice.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/slice/target/debug/slice: /mnt/c/Users/kdh95/Desktop/RustStudy/slice/src/main.rs
