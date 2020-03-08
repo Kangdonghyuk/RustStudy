2fe9d26561267c7d6ac8046e9e23654a0c67ac77
Kangdonghyuk <kdh950812@gmail.com>
2020-03-08 19:31:51 +0900

    0308-add what ownership

---
+++ ownership_1/target/debug/deps/ownership_1-be11eacfbdea2430.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/ownership_1/target/debug/deps/ownership_1-be11eacfbdea2430: src/main.rs
+
+/mnt/c/Users/kdh95/Desktop/RustStudy/ownership_1/target/debug/deps/ownership_1-be11eacfbdea2430.d: src/main.rs
+
+src/main.rs:
---
+++ ownership_1/target/debug/ownership_1.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/ownership_1/target/debug/ownership_1: /mnt/c/Users/kdh95/Desktop/RustStudy/ownership_1/src/main.rs
