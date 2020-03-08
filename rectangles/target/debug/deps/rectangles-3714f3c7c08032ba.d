61d6854cdb31bad54f1b5505f3f7f9a99dc9f8d8
Kangdonghyuk <kdh950812@gmail.com>
2020-03-08 21:00:03 +0900

    0308-add example struct and struct method

---
+++ rectangles/target/debug/deps/rectangles-3714f3c7c08032ba.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/rectangles/target/debug/deps/rectangles-3714f3c7c08032ba: src/main.rs
+
+/mnt/c/Users/kdh95/Desktop/RustStudy/rectangles/target/debug/deps/rectangles-3714f3c7c08032ba.d: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/kdh95/Desktop/RustStudy/rectangles/target/debug/rectangles: /mnt/c/Users/kdh95/Desktop/RustStudy/rectangles/src/main.rs
